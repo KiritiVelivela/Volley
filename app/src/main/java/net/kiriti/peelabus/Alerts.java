9a401baf710a20b253dd700861e75077fd46e247
KiritiVelivela <skvelivela@gmail.com>
2017-11-10 00:25:05 +0530

    UI all except childprofile

---
+++ app/src/main/java/net/kiriti/peelabus/Alerts.java
@@ -0,0 +1,13 @@
+package net.kiriti.peelabus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alerts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alerts);
+    }
+}
